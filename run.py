3ba90f4168c2b391e0bf5f2db21554c3fe82f042
praneel <u.praneel.nihar@gmail.com>
2020-01-05 21:58:01 +0530

    modifies run.py.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from termcolor import colored
 
 session = Session()
 
-print(colored('\033[1m' + 'BOT:','red'), colored('\033[1m'+'Hi! I can help you find restaurants and movies. What is your preference?','green'))
+print(colored('\033[1m' + 'BOT:','red'), colored('\033[1m'+'Hi! I can help you find movies and book restaurants. What is your preference?','green'))
 
 while True:
     inp = input('User: ') or 'default'
