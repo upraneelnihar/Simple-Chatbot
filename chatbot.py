4039e08e82415a2507d8df66dc5afe17d940ff83
praneel <u.praneel.nihar@gmail.com>
2020-01-05 21:22:57 +0530

    adds chatbot module.

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+'''
+Module for chatbot.
+This module implements the various tasks performed by chatbot after a
+user input  which are listed below:
+- Input Processing
+- Intent identification
+- Parameter check for current intent
+- Perform action.
+'''
